8f4191cd52738d6540d10a262bae2c62e33af259
Lohi26 <lohitachandru@gmail.com>
2024-02-28 10:41:35 +0530

    days

--- indexjs.js
+++ indexjs.js
@@ -1,1 +1,14 @@
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
 
+
+    document.querySelector("html").addEventListener("click",function(){
+        header.style.color="blue";
+    });
+})();
+
+
+//this above function complete works based on the closure here
+//the first  thought will raise when we use header variable from one scope to the another 
+//scope but that works perfectly based on the closure concept here 
